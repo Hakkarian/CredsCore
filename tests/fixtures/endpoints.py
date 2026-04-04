92cb3f8e3edbb552cb038f4c9dc18c5fe632d2ed
Hakkarian <theobadar@gmail.com>
2026-04-04 18:16:05 +0300

    feat: test fixtures

---
+++ tests/fixtures/endpoints.py
@@ -0,0 +1,40 @@
+SAMPLE_APPLICANT = {
+    "RevolvingUtilizationOfUnsecuredLines": 0.766127,
+    "age": 45,
+    "NumberOfTime30_59DaysPastDueNotWorse": 2,
+    "DebtRatio": 0.802982,
+    "MonthlyIncome": 9120.0,
+    "NumberOfOpenCreditLinesAndLoans": 13,
+    "NumberOfTimes90DaysLate": 0,
+    "NumberRealEstateLoansOrLines": 6,
+    "NumberOfTime60_89DaysPastDueNotWorse": 0,
+    "NumberOfDependents": 2
+}
+
+HIGH_RISK_APPLICANT = {
+    "RevolvingUtilizationOfUnsecuredLines": 1.2,
+    "age": 25,
+    "NumberOfTime30_59DaysPastDueNotWorse": 5,
+    "DebtRatio": 2.5,
+    "MonthlyIncome": 2000.0,
+    "NumberOfOpenCreditLinesAndLoans": 3,
+    "NumberOfTimes90DaysLate": 3,
+    "NumberRealEstateLoansOrLines": 0,
+    "NumberOfTime60_89DaysPastDueNotWorse": 2,
+    "NumberOfDependents": 0
+}
+
+LOW_RISK_APPLICANT = {
+    "RevolvingUtilizationOfUnsecuredLines": 0.1,
+    "age": 55,
+    "NumberOfTime30_59DaysPastDueNotWorse": 0,
+    "DebtRatio": 0.3,
+    "MonthlyIncome": 15000.0,
+    "NumberOfOpenCreditLinesAndLoans": 10,
+    "NumberOfTimes90DaysLate": 0,
+    "NumberRealEstateLoansOrLines": 2,
+    "NumberOfTime60_89DaysPastDueNotWorse": 0,
+    "NumberOfDependents": 3
+}
+
+BASE_URL = "http://localhost:8000"
